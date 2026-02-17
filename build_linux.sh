0870f53c5c7fe5a981c7a69a481b151f541f3add
Alex M. <alex.marquardt@gmail.com>
2026-02-17 17:41:07 +0100

    add linux enviroment WIP

--- build_linux.sh
+++ build_linux.sh
@@ -1,2 +1,2 @@
-docker build --platform linux/amd64 -f Dockerfile.linux-build -t wails-linux-builder .
-docker run --platform linux/amd64 --rm -v "$PWD:/src" -w /src wails-linux-builder
+wails build -platform linux/amd64
+nfpm package -f nfpm.yaml -p deb -t build/bin/
